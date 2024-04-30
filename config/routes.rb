eb22478b9f2b4375fad08b9af8300e6a9a49c6d2
mogami <ndarfc.57@gmail.com>
2024-04-30 20:38:07 +0900

    [fix]ヘッダーのリンクを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'top#index'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'top#index'
+  get '/dashboard' => 'dashboard#index'
 end
