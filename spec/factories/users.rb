c73c43724057a2eaadf5fc96be1f34e1d1ac93d8
mogami <ndarfc.57@gmail.com>
2024-05-24 23:53:24 +0900

    [add]userのfactorybotを設定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username { Faker::Name.unique.name }
+    email { Faker::Internet.unique.email }
+    password { '12345678' }
+    password_confirmation { '12345678' }
   end
 end
