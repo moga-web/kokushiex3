cd96dc95a56705de67dd54aed5a47dcd821d1b1a
mogami <ndarfc.57@gmail.com>
2024-06-02 21:44:50 +0900

    [add]sign_inのリクエストスペックを作った

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,7 +15,8 @@ RSpec.describe "Users", type: :request do
         expect {
           post user_registration_path, params: user_params
         }.to change(User, :count).by(1)
-        expect(response).to redirect_to(dashboard_path) 
+        expect(response).to redirect_to(dashboard_path)
+        expect(response).to have_http_status(:see_other)#Rails7では、POSTリクエスト後のリダイレクトは303
       end
     end
 
@@ -26,6 +27,29 @@ RSpec.describe "Users", type: :request do
           post user_registration_path, params: user_params
         }.not_to change(User, :count)
         expect(response).to render_template(:new)
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe "POST /users/sign_in" do
+    context '正常系' do
+      it 'ログイン後ダッシュボード画面にリダイレクトされる' do
+        user = User.create(username: 'testuser', email: 'testuser@example.com', password: 'password', password_confirmation: 'password')
+        user_params = { user: { email: 'testuser@example.com', password: 'password' } }
+        post user_session_path, params: user_params
+        expect(response).to redirect_to(dashboard_path)
+        expect(response).to have_http_status(:see_other)
+      end
+    end
+
+    context '不正なパラメータの場合' do
+      it 'ログインができずログイン画面にリダイレクトされる' do
+        user = User.create(username: 'testuser', email: 'testuser@example.com', password: 'password', password_confirmation: 'password')
+        user_params = { user: { email: 'testuser@example.com', password: '123456789' } }
+        post user_session_path, params: user_params
+        expect(response).to render_template(:new)
+        expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
