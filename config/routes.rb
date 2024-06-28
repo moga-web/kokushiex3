9afafae65fcadf278bcaa6f67709196cacd40382
mogami <ndarfc.57@gmail.com>
2024-06-29 08:36:53 +0900

    [Add]letter_opener_webをのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   
   root to: 'top#index'
   get '/dashboard' => 'dashboard#index'
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
