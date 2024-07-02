9a9982437b8d003c4d0b21a1b77cfb34f46dcefa
mogami <ndarfc.57@gmail.com>
2024-07-02 20:05:35 +0900

    [fix]不要な差分を修正

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("Hello_controller connected")
+    console.log("Hello_Controller connected")
   }
 }
