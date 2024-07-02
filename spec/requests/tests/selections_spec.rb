cc604b02ae8f907493109daa758285073789994f
mogami <ndarfc.57@gmail.com>
2024-07-02 19:52:38 +0900

    [fix]tabの切り替えをtoggleをやめた

--- spec/requests/tests/selections_spec.rb
+++ spec/requests/tests/selections_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "Tests::Selections", type: :request do
   describe "GET /index" do
-    it "returns http success" do
-      get "/tests/selections/index"
+    it 'returns http success' do
+      get '/select'
       expect(response).to have_http_status(:success)
     end
   end
