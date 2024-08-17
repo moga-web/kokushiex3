91dc356627e2d3158f488ff163f79877dd1d671f
mogami <ndarfc.57@gmail.com>
2024-08-17 21:47:28 +0900

    [Add]hidden_field_tagを追加

--- app/controllers/user_responses_controller.rb
+++ app/controllers/user_responses_controller.rb
@@ -1,3 +1,21 @@
 class UserResponsesController < ApplicationController
-  def create; end
+  def create
+    @examination = Examination.new(
+      user_id: current_user.id,
+      test_id: params[:test_id],
+      attempt_date: Datetime.current
+    )
+
+    params[:user_responses].each do |question_id, choice_ids|
+      choice_ids.each do |choice_id|
+        UserResponse.create!(
+          examination_id: @examination.id,
+          choice_id: choice_id
+        )
+      end
+    end
+
+    redirect_to dashboard_path, notice: '試験結果を保存しました'
+  end
+  end
 end
