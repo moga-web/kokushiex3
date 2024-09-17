a3521b62083569b2133ca6a383daad1bbcd793b2
mogami <ndarfc.57@gmail.com>
2024-09-17 21:49:02 +0900

    [fix]]リクエストスペックとコントローラを修正

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,7 +2,8 @@ class TestsController < ApplicationController
   def show
     @test = Test.find(params[:id]).decorate
     @test_sessions = @test.test_sessions.includes(questions: :choices)
-    @questions = @test_sessions.questions.includes(:choices)
+    # 各TestSessionに紐づくquestionsを全て取得する
+    @questions = @test_sessions.flat_map(&:questions)
     # 解答を保持するためにuser_responseを持たせたいが、初回にエラーが出ないように空配列を渡す
     @user_responses = []
   end
--- spec/factories/test_sessions.rb
+++ spec/factories/test_sessions.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: test_sessions
+#
+#  id         :bigint           not null, primary key
+#  session    :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  test_id    :bigint           not null
+#
+# Indexes
+#
+#  index_test_sessions_on_test_id  (test_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (test_id => tests.id)
+#
 FactoryBot.define do
   factory :test_session do
     association :test
