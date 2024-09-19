9be89bd1ff8f4efe161967441b325b6dbbf91687
mogami <ndarfc.57@gmail.com>
2024-09-19 17:04:21 +0900

    annotateを実行

--- app/models/pass_mark.rb
+++ app/models/pass_mark.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: pass_marks
+#
+#  id                       :bigint           not null, primary key
+#  required_practical_score :integer          not null
+#  required_score           :integer          not null
+#  total_score              :integer          not null
+#  created_at               :datetime         not null
+#  updated_at               :datetime         not null
+#  test_id                  :bigint           not null
+#
+# Indexes
+#
+#  index_pass_marks_on_test_id  (test_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (test_id => tests.id)
+#
 class PassMark < ApplicationRecord
   belongs_to :test
 
