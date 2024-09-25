c1326a5157fa0690291e7959e14d08bb102b6431
mogami <ndarfc.57@gmail.com>
2024-09-25 23:16:01 +0900

    [fix]scores#createでのparamsの受け取り方を修正

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -8,12 +8,14 @@ class ScoresController < ApplicationController
     redirect_to examination_path(@examination), alert: 'スコアが存在しません。'
   end
 
-  def create(examination_id, scores)
-    examination = Examination.find(examination_id)
+  def create
+    examination = Examination.find(params[:examination_id])
+    score_params = params.require(:scores).permit(:total, :common, :practical)
+
     score = examination.build_score(
-      total_score: scores[:total],
-      common_score: scores[:common],
-      practical_score: scores[:practical]
+      total_score: score_params[:total],
+      common_score: score_params[:common],
+      practical_score: score_params[:practical]
     )
     Rails.logger.error "スコアの登録に失敗しました: #{score.errors.full_messages.join(', ')}" unless score.save
   end
--- spec/requests/scores_spec.rb
+++ spec/requests/scores_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Scores", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /examinations/:examination_id/scores" do
+    let(:user) { create(:user) }
+    let(:examination) { create(:examination) }
+    let(:score_params) do
+      {
+        total: 300,
+        common: 120,
+        practical: 180
+      }
+    end
+
+    before do
+      sign_in user
+    end
+
+    it 'creates a new score and returns http success' do
+      post examination_scores_path(examination.id), params: { scores: score_params }
+      expect(response).to have_http_status(:success)
+    end
   end
 end
