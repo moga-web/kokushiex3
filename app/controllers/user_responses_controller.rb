911fc4c5f7921cb0ab076535d3e1d9b7068dbde8
mogami <ndarfc.57@gmail.com>
2024-09-27 19:02:24 +0900

    [fix]user_responses_specにjobのenqueに関するテストを追加

--- app/controllers/user_responses_controller.rb
+++ app/controllers/user_responses_controller.rb
@@ -6,7 +6,7 @@ class UserResponsesController < ApplicationController
     if UserResponse.bulk_create_responses(@examination.id, params[:user_response][:choice_ids])
       # スコア計算ジョブをキューに追加
       # insert_allではafter_saveを指定できない
-      ScoreCalculationJob.perform_later(examination_id)
+      ScoreCalculationJob.perform_later(@examination.id)
       redirect_to dashboard_path, notice: '試験結果を保存しました'
     else
       @user_responses = params[:user_response][:choice_ids]
