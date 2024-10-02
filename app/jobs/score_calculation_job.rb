142891c9ac9cfa2d20763c693002c1c5664b382d
mogami <ndarfc.57@gmail.com>
2024-10-02 14:44:11 +0900

    [fix]MySQL doesn't yet support 'LIMIT & IN/ALL/ANY/SOME subqueryとなるのでpluckを使用しIDを事前に配列として取得するようにした

--- app/jobs/score_calculation_job.rb
+++ app/jobs/score_calculation_job.rb
@@ -30,12 +30,13 @@ class ScoreCalculationJob < ApplicationJob
 
   # 実地スコアの計算 (1問3点)
   def calculate_practical_score(examination, practical_questions)
-    examination.user_responses.joins(:choice).where(choices: { is_correct: true },
-                                                    question: practical_questions).count * 3
+    practical_question_ids = practical_questions.pluck(:id)
+    examination.user_responses.joins(:choice).where(choices: { is_correct: true }, question_id: practical_question_ids).count * 3
   end
 
   # 共通スコアの計算 (1問1点)
   def calculate_common_score(examination, common_questions)
-    examination.user_responses.joins(:choice).where(choices: { is_correct: true }, question: common_questions).count * 1
+    common_question_ids = common_questions.pluck(:id)
+    examination.user_responses.joins(:choice).where(choices: { is_correct: true }, question_id: common_question_ids).count * 1
   end
 end
