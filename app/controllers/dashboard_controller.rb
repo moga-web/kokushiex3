5081aa6b297d1ff1b2c31e41b9ecfe0556a557c5
mogami <ndarfc.57@gmail.com>
2024-10-06 22:34:24 +0900

    [add]実施年度を表示できるようにした

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,3 +1,8 @@
 class DashboardController < ApplicationController
-  def index; end
+  def index
+    # current_userの最新のexamaminationを取得
+    @latest_examination = current_user.examinations.order(created_at: :desc).first
+    
+    # ... 他のコード ...
+  end
 end
--- app/decorators/test_decorator.rb
+++ app/decorators/test_decorator.rb
@@ -2,8 +2,15 @@ class TestDecorator < Draper::Decorator
   delegate_all
 
   def question_code(question)
-    implementation_year = object.year.to_i - 1965
     abbreviated_session = question.test_session.session == 'AM' ? 'A' : 'P'
-    "#{implementation_year}#{abbreviated_session}#{question.question_number}"
+    "#{turn}#{abbreviated_session}#{question.question_number}"
+  end
+
+  def turn
+    object.year.to_i - 1965
+  end
+
+  def implementation_year
+    "第#{turn}回(#{object.year}年度)"
   end
 end
