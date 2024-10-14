b12f39eafea44a76190bd7b74c0b6a58d715bf29
mogami <ndarfc.57@gmail.com>
2024-10-14 21:47:08 +0900

    [fix]スコアの計算と問題の分類を一度に行うように修正した

--- app/models/score.rb
+++ app/models/score.rb
@@ -25,19 +25,12 @@ class Score < ApplicationRecord
   validates :common_score, presence: true
   validates :practical_score, presence: true
 
-  def calculate_scores(examination) # rubocop:disable Metrics/MethodLength
+  def calculate_scores(examination)
     scores = { total_score: 0, common_score: 0, practical_score: 0 }
-    taeget_test_sessions = examination.test.test_sessions
-
-    taeget_test_sessions.each do |test_session|
-      # 　配点に応じた問題に分ける
-      sorted_questions = Question.sort_questions(test_session)
-      practical_questions = sorted_questions[:practical_questions]
-      common_questions = sorted_questions[:common_questions]
-      # 配点ごとにスコアを計算
-      scores[:practical_score] += calculate_practical_score(examination, practical_questions)
-      scores[:common_score] += calculate_common_score(examination, common_questions)
-    end
+    correct_responses = preload_responses(examination)
+    # 配点ごとにスコアを計算
+    scores[:practical_score] += calculate_practical_score(correct_responses)
+    scores[:common_score] += calculate_common_score(correct_responses)
     scores[:total_score] = scores[:practical_score] + scores[:common_score]
 
     update!(
@@ -47,15 +40,17 @@ class Score < ApplicationRecord
     )
   end
 
+  def preload_responses(examination)
+    examination.user_responses.includes(choice: :question).where(choices: { is_correct: true }).to_a
+  end
+
   # 実地スコアの計算 (1問3点)
-  def calculate_practical_score(examination, practical_question_ids)
-    examination.user_responses.includes(choice: :question).where(choices: { is_correct: true },
-                                                                 questions: { id: practical_question_ids }).count * 3
+  def calculate_practical_score(correct_responses)
+    correct_responses.count { |response| response.choice.question.question_number <= 20 } * 3
   end
 
   # 共通スコアの計算 (1問1点)
-  def calculate_common_score(examination, common_question_ids)
-    examination.user_responses.includes(choice: :question).where(choices: { is_correct: true },
-                                                                 questions: { id: common_question_ids }).count * 1
+  def calculate_common_score(correct_responses)
+    correct_responses.count { |response| response.choice.question.question_number > 20 } * 1
   end
 end
