5c8fd4e875ee329565b9adf5e5c7d5cad367124a
mogami <ndarfc.57@gmail.com>
2024-10-16 18:25:07 +0900

    [fix]問題番号の表示を変更

--- app/decorators/test_decorator.rb
+++ app/decorators/test_decorator.rb
@@ -6,6 +6,11 @@ class TestDecorator < Draper::Decorator
     "#{turn}#{abbreviated_session}#{question.question_number}"
   end
 
+  def question_code_without_turn(question)
+    abbreviated_session = question.test_session.session == 'AM' ? 'A' : 'P'
+    "#{abbreviated_session}#{question.question_number}"
+  end
+
   def turn
     object.year.to_i - 1965
   end
