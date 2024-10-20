27876138e55b3f2190812a4dae71917435c7aab1
mogami <ndarfc.57@gmail.com>
2024-10-21 00:14:01 +0900

    [fix]questionsが50でnilが返らないように空配列を渡すようにした

--- spec/requests/examinations_spec.rb
+++ spec/requests/examinations_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "Examinations", type: :request do
   describe "GET /examinations/:id" do
+    let(:test){create(:test)}
+    let(:test_session) { create(:test_session, test:) }
+    let(:examination) { create(:examination, test:) }
+    let!(:pass_mark){ create(:pass_mark, test:)}
+    let!(:score) { create(:score, examination:) }
+    let!(:question){ create(:question, test_session:) }
+    let!(:choice) { create(:choice, question:) }
+    let!(:user_response) { create(:user_response, examination:, choice:) }
+
     it "returns http success" do
-      get examination_path
+      get examination_path(examination)
       expect(response).to have_http_status(200)
     end
   end
