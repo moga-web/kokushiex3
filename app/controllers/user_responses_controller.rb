89decf1becce883cc9a338485ddf45f2593108a2
mogami <ndarfc.57@gmail.com>
2024-10-31 20:28:28 +0900

    [fix]Score::ScoreCalculatorのテストを修正

--- app/controllers/user_responses_controller.rb
+++ app/controllers/user_responses_controller.rb
@@ -4,12 +4,11 @@ class UserResponsesController < ApplicationController
       Examination.create_result!(user_id: current_user.id,
                                  test_id: params[:test_id],
                                  attempt_date: DateTime.current,
-                                 choice_ids: params[:user_response][:choice_ids]
-                                )
+                                 choice_ids: params[:user_response][:choice_ids])
       # リダイレクト
       redirect_to dashboard_path, notice: '試験結果を保存しました'
     rescue StandardError => e
-      @user_responses = params[:user_response][:choice_ids]
+      logger.error("試験結果の保存エラー: #{e.message}")
       render :new, alert: '試験結果を保存できませんでした'
     end
   end
