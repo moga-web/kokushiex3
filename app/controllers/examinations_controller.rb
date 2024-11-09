5114947ad6fc4513d51b65042371a3091efbc437
mogami <ndarfc.57@gmail.com>
2024-11-09 19:03:09 +0900

    [fix]コントローラでcurrent_userに絞った

--- app/controllers/examinations_controller.rb
+++ app/controllers/examinations_controller.rb
@@ -1,6 +1,6 @@
 class ExaminationsController < ApplicationController
   def show
-    @examination = Examination.find(params[:id])
+    @examination = current_user.examinations.find(params[:id])
     @score = @examination.score
     @test = @examination.test
     # 参考：https://zenn.dev/mithuami/articles/c4b0e9694182d1#preload%E3%82%92%E6%8E%A8%E5%A5%A8%E3%81%99%E3%82%8B%E3%82%B1%E3%83%BC%E3%82%B9
