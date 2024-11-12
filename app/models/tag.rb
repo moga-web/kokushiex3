12f9f2a928c42fcf1f73ebb5319bc3292b7494f8
mogami <ndarfc.57@gmail.com>
2024-11-12 21:23:18 +0900

    [add]それぞれのタグをみやすくした

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -16,4 +16,8 @@ class Tag < ApplicationRecord
   has_many :questions, through: :question_tags
 
   validates :name, presence: true, uniqueness: true
+
+  scope :common_tags, -> { where(id: 4..13) }
+  scope :special_tags, -> { where(id: 14..26) }
+  scope :major_category, -> { where(id: 1..3) }
 end
