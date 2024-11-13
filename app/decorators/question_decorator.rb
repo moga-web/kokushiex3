5c1df07fa75070f87a9c4c6fe6eeb43be7cd7eea
mogami <ndarfc.57@gmail.com>
2024-11-13 22:28:18 +0900

    [fix]色が反映されない場合があるので正解の場合も色を返すようにした

--- app/decorators/question_decorator.rb
+++ app/decorators/question_decorator.rb
@@ -4,9 +4,9 @@ class QuestionDecorator < Draper::Decorator
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
-  def incorrect_color(examination)
+  def result_color(examination)
     return 'text-red-300' if selected_option_numbers(examination).empty?
 
-    selected_option_numbers(examination).sort == correct_option_numbers.sort ? '' : 'text-red-300'
+    selected_option_numbers(examination).sort == correct_option_numbers.sort ? 'text-gray-800' : 'text-red-300'
   end
 end
