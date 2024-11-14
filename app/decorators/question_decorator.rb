9059c938fc50d96d2dc85146d144891bf156ffca
mogami <ndarfc.57@gmail.com>
2024-11-14 22:08:09 +0900

    [add]問題の右上にタグを表示するようにした

--- app/decorators/question_decorator.rb
+++ app/decorators/question_decorator.rb
@@ -1,9 +1,5 @@
 class QuestionDecorator < Draper::Decorator
   delegate_all
-
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
   def result_color(examination)
     return 'text-red-300' if selected_option_numbers(examination).empty?
 
