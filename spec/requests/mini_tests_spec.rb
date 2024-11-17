296f1f98ec1c880504bbc48283eea3b8e647e217
mogami <ndarfc.57@gmail.com>
2024-11-17 14:52:29 +0900

    [add]リクエストスペックを追加

--- spec/requests/mini_tests_spec.rb
+++ spec/requests/mini_tests_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "MiniTests", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    let(:tag) { create(:tag) }
+    let(:question) { create(:question, tags: [tag]) }
+
+    it "returns http success" do
+      get mini_tests_path(q: { tag_ids: [tag.id] })
+      expect(response).to have_http_status(:success)
+    end
   end
 end
