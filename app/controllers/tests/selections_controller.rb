7797439fb38c772aa8dc1ff7d32615432192d481
mogami <ndarfc.57@gmail.com>
2024-11-19 23:58:06 +0900

    [fix]とりあえず問題を絞れるようにしてみた

--- app/controllers/tests/selections_controller.rb
+++ app/controllers/tests/selections_controller.rb
@@ -4,5 +4,14 @@ class Tests::SelectionsController < ApplicationController
     @common_tags = Tag.common_tags
     @special_tags = Tag.special_tags
     @major_categorys = Tag.major_category
+
+    @questions = if params[:tag_ids].present?
+                   tag_ids = params[:tag_ids]
+                   Question.joins(:question_tags)
+                           .where(question_tags: { tag_id: tag_ids })
+                           .distinct
+                 else
+                   Question.all
+                 end
   end
 end
