09e5fbd3bb46650d2c824f29c259e3639f9c2fb4
mogami <ndarfc.57@gmail.com>
2024-11-21 08:47:46 +0900

    [add]問題数を指定できるようにした

--- app/controllers/mini_tests_controller.rb
+++ app/controllers/mini_tests_controller.rb
@@ -1,13 +1,21 @@
 class MiniTestsController < ApplicationController
   def index
-    # 問題数も選択できるようにする
-    # 　必要なメソッドはモデルに移す
-    @questions = if params[:tag_ids].present?
-                   tag_ids = params[:tag_ids]
-                   Question.joins(:question_tags)
-                           .where(question_tags: { tag_id: tag_ids })
-                           .distinct
-                 end
+    tag_ids = params[:tag_ids] || []
+    # タグに基づいて質問を取得
+    @questions = Question.joins(:question_tags)
+                         .where(question_tags: { tag_id: tag_ids })
+                         .distinct
+
+    # 質問のIDリストを取得
+    question_ids = @questions.pluck(:id) # IDの配列を取得
+
+    # ランダムに選択する問題数を取得
+    question_count = params[:question_count].to_i
+
+    # ランダムに指定された数のIDを選択
+    random_ids = question_ids.sample(question_count)
+    @questions = @questions.where(id: random_ids) # 選択したIDに基づいて質問をフィルタリング
+
     @user_responses = []
   end
 end
