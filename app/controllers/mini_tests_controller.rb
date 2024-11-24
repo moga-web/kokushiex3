a832580f0554cb5e670cb21266ea3283d3d864a9
dyson <hrkedz@gmail.com>
2024-11-24 09:34:52 +0900

    mini_test_search_form

--- app/controllers/mini_tests_controller.rb
+++ app/controllers/mini_tests_controller.rb
@@ -1,16 +1,6 @@
 class MiniTestsController < ApplicationController
   def index
-    # TODO: Formオブジェクト化する
-    tag_ids = params[:tag_ids] || []
-    # タグに基づいて質問のIDリストを配列で取得
-    question_ids = Question.joins(:question_tags)
-                           .where(question_tags: { tag_id: tag_ids })
-                           .distinct
-                           .pluck(:id)
-    # ランダムに選択する問題数を指定
-    question_count = params[:question_count].to_i
-    @questions = Question.random_questions(question_ids, question_count)
-
+    @questions = MiniTestSearchForm.new(params).search
     @user_responses = []
   end
 end
---
+++ app/forms/mini_test_search_form.rb
@@ -0,0 +1,22 @@
+class MiniTestSearchForm
+  include ActiveModel::Model
+  include ActiveModel::Attributes
+
+  attr_accessor :tag_ids, :question_count
+
+  validates :tag_ids, presence: true
+  validates :question_count, presence: true
+
+  def initialize(params)
+    @tag_ids = params[:tag_ids] || []
+    @question_count = params[:question_count].to_i
+  end
+
+  def search
+    question_ids = Question.joins(:question_tags)
+                           .where(question_tags: { tag_id: @tag_ids })
+                           .distinct
+                           .pluck(:id)
+    Question.random_questions(question_ids, @question_count)
+  end
+end
