9e04497772752d3958bd93b6a4ac54f15f5ce455
mogami <ndarfc.57@gmail.com>
2024-11-26 07:07:58 +0900

    [fix]initializeは不要だった

--- app/controllers/mini_tests_controller.rb
+++ app/controllers/mini_tests_controller.rb
@@ -1,11 +1,17 @@
 class MiniTestsController < ApplicationController
   def index
-    form = MiniTestSearchForm.new(params[:search])
+    form = MiniTestSearchForm.new(params)
     if form.valid?
       @questions = form.search
       @user_responses = []
     else
       redirect_to tests_select_path, alert: form.errors.full_messages.join(', ')
     end
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(tag_ids: [], question_count: nil)
   end
 end
--- app/forms/mini_test_search_form.rb
+++ app/forms/mini_test_search_form.rb
@@ -2,24 +2,17 @@ class MiniTestSearchForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
-  attr_accessor :tag_ids, :question_count
-
-  attribute :tag_ids, :integer
-  attribute :question_count, :integer
+  attribute :tag_ids, default: []
+  attribute :question_count, :integer, default: 10
 
   validates :tag_ids, presence: { message: 'タグを選択してください' }
   validates :question_count, presence: true
 
-  def initialize(params)
-    @tag_ids = params[:tag_ids] || []
-    @question_count = params[:question_count].to_i || 10
-  end
-
   def search
     question_ids = Question.joins(:question_tags)
-                           .where(question_tags: { tag_id: @tag_ids })
+                           .where(question_tags: { tag_id: tag_ids })
                            .distinct
                            .pluck(:id)
-    Question.random_questions(question_ids, @question_count)
+    Question.random_questions(question_ids, question_count)
   end
 end
