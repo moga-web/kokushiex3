169065831e59d2aaeebd210a18f1fdfaecf5174a
mogami <ndarfc.57@gmail.com>
2024-12-01 21:18:52 +0900

    [fix]helperからjoinを削除

--- app/helpers/mini_tests_helper.rb
+++ app/helpers/mini_tests_helper.rb
@@ -2,6 +2,6 @@ module MiniTestsHelper
   def correct_answer_icon(selected_answers, question)
     selected_answers = selected_answers[question.id]
     correct_answers = question.correct_option_numbers
-    selected_answers&.join(', ') == correct_answers.join(', ') ? '⭕️' : '❌'
+    selected_answers == correct_answers ? '⭕️' : '❌'
   end
 end
