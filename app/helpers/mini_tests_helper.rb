7f6a60dc8bd35671ed7520eec548a7f3616e7084
mogami <ndarfc.57@gmail.com>
2024-12-01 10:58:24 +0900

    [fix]ボタンも差し替えるようにした

---
+++ app/helpers/mini_tests_helper.rb
@@ -0,0 +1,7 @@
+module MiniTestsHelper
+  def correct_answer_icon(selected_answers, question)
+    selected_answers = selected_answers[question.id]
+    correct_answers = question.correct_option_numbers
+    selected_answers&.join(', ') == correct_answers.join(', ') ? '⭕️' : '❌'
+  end
+end
