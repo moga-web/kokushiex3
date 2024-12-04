70398f83199e1d07fc8c96bbc8e62f10f2375057
mogami <ndarfc.57@gmail.com>
2024-12-04 20:34:13 +0900

    [fix]turboで置き換える部分を切り出した

--- app/controllers/mini_tests_controller.rb
+++ app/controllers/mini_tests_controller.rb
@@ -9,20 +9,14 @@ class MiniTestsController < ApplicationController
     end
   end
 
-  def create # rubocop:disable Metrics/MethodLength
+  def create
     @selected_answers = Choice.where(id: params[:user_response][:choice_ids])
                               .group_by(&:question_id)
                               .transform_values { |choices| choices.map(&:option_number) }
     @questions = Question.where(id: params[:question_ids])
 
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace('mini_test_answers',
-                                                  partial: 'mini_tests/answers',
-                                                  locals: { selected_answers: @selected_answers,
-                                                            questions: @questions })
-        format.html
-      end
+      format.turbo_stream
     end
   end
 end
