654d5e0f6324768b56fcc8befbd03401fbfacfd5
mogami <ndarfc.57@gmail.com>
2024-12-05 20:19:50 +0900

    [fix]tailwindが反映されるように戻した

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+   # better_errorsをdocker環境で許可
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -42,7 +43,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
-
+  # Letter Opener
+  config.action_mailer.delivery_method = :letter_opener_web
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -63,7 +65,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
+  config.assets.debug = true
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
