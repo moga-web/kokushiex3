4ed5e5542fb0f6fe9335779f247f552810e09838
mogami <ndarfc.57@gmail.com>
2024-12-06 23:03:16 +0900

    [fix]テストにloginを追加

--- spec/requests/tests/selections_spec.rb
+++ spec/requests/tests/selections_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Tests::Selections' do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
       get '/tests/select'
