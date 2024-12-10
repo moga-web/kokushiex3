3840eafab72e2428796013f6c9f6810379e4ed2f
mogami <ndarfc.57@gmail.com>
2024-12-11 00:18:04 +0900

    [fix]タイポを修正

--- render/entrypoint.sh
+++ render/entrypoint.sh
@@ -7,4 +7,4 @@ rm -f /myapp/tmp/pids/server.pid
 bundle exec rails db:migrate
 bundle exec rails db:seed_fu
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@
+exec "$@"
