707ce89f0108dabc08a572cf57100abdc480df47
mogami <ndarfc.57@gmail.com>
2024-12-11 01:05:33 +0900

    [fix] assets:cleanで直近３バージョンまでに絞る

--- render/entrypoint.sh
+++ render/entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 rm -f /myapp/tmp/pids/server.pid
 
 bundle exec rails assets:precompile
+bundle exec rails assets:clean
 bundle exec rails db:migrate
 bundle exec rails db:seed_fu
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
