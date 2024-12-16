6a07c09fe86fa24fb99fa7ff0786f54494ae80fb
mogami <ndarfc.57@gmail.com>
2024-12-16 18:39:06 +0900

    [fix]staging環境のseedの読み込みのファイルパスを指定

--- render/entrypoint.sh
+++ render/entrypoint.sh
@@ -7,6 +7,6 @@ rm -f /myapp/tmp/pids/server.pid
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-bundle exec rails db:seed_fu
+bundle exec rails db:seed_fu FIXTURE_PATH=db/fixtures/staging
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
